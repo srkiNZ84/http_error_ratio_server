ca49922e40a73dad65a2df6d57783fd6c8143a77
Serge Dukic <serge.dukic@clearpoint.co.nz>
2021-02-16 22:22:16 +1300

    First commit
    Initial cut of the server code.
    
    Issue(s): None

--- main.go
+++ main.go
@@ -2,30 +2,58 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
 	"time"
 )
 
-var ration int = 30
+var rng rand.Rand
+var ratio int = 30
+var errCounter int = 0
+var totalCount int = 0
 
 func main() {
+	log.Println("Seeding random number generator...")
 	source := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(source)
+	rng = *rand.New(source)
 
-	rv := []int{}
-	var downCounter int = 0
-	for i := 0; i < 100; i++ {
-		t := r.Intn(100)
-		rv = append(rv, t)
-		if t >= ration {
-			fmt.Println("200 OK")
-		} else {
-			downCounter++
-			fmt.Println("503 Service Unavailable")
+	log.Println("Starting server...")
+	http.HandleFunc("/", handler)
 
-		}
+	ratio, _ := strconv.Atoi(os.Getenv("ERROR_RATIO"))
+	if ratio == 0 {
+		ratio = 30
+		log.Printf("Default ratio of %v percent will be used", ratio)
 	}
 
-	fmt.Println("Here it is", rv)
-	fmt.Println("Downcounter is", downCounter)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Configuring default port %s", port)
+	}
+
+	log.Printf("Starting to listen on port %s", port)
+	log.Printf("%v percent of requests will return an HTTP 503", ratio)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal("Error starting http server", err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	num := rng.Intn(100)
+	if num >= ratio {
+		w.WriteHeader(http.StatusOK)
+		totalCount++
+		fmt.Fprint(w, "Everything's fine!")
+		fmt.Fprintf(w, "\n\nPercent errors returned so far is %v", float32(errCounter)/float32(totalCount))
+	} else {
+		w.WriteHeader(http.StatusServiceUnavailable)
+		errCounter++
+		totalCount++
+		fmt.Fprint(w, "Server Error!")
+		fmt.Fprintf(w, "\n\nPercent errors returned so far is %v", float32(errCounter)/float32(totalCount))
+	}
 }
