5660f201e3c336c1fd80ca8367b59652c1ec0b4c
Serge Dukic <serge.dukic@clearpoint.co.nz>
2021-10-16 23:47:15 +1300

    Refactor the responses as middleware
    After deliberation it makes sense to make these http middleware.
    
    Issue(s): None

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module serge.sh/http_error_ratio_server
 
 go 1.17
+
+require github.com/justinas/alice v1.2.0 // indirect
--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/justinas/alice"
 )
 
 var rng = *rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -16,6 +18,7 @@ var slow bool = true
 var slow_duration = 2
 var errCounter int = 0
 var totalCount int = 0
+var port string = "8080"
 
 const (
 	success_message = "Everything's fine!"
@@ -24,11 +27,19 @@ const (
 )
 
 func main() {
+	configure()
+
+	chain := alice.New(randomHandler, slowHandler).Then(http.HandlerFunc(myHandler))
 
 	log.Println("Starting server...")
-	//http.HandleFunc("/", handler)
-	http.HandleFunc("/", randomHandler(slowHandler()))
+	log.Printf("Starting to listen on port %s", port)
+	log.Printf("Approximately %v percent of requests will return an HTTP 503", ratio)
+	if err := http.ListenAndServe(":"+port, chain); err != nil {
+		log.Fatal("Error starting http server", err)
+	}
+}
 
+func configure() {
 	ratio, _ := strconv.Atoi(os.Getenv("ERROR_RATIO"))
 	if ratio == 0 {
 		ratio = 30
@@ -46,12 +57,6 @@ func main() {
 		port = "8080"
 		log.Printf("Configuring default port %s", port)
 	}
-
-	log.Printf("Starting to listen on port %s", port)
-	log.Printf("Approximately %v percent of requests will return an HTTP 503", ratio)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal("Error starting http server", err)
-	}
 }
 
 // Interface wrapper around things that can return a random integer
@@ -66,26 +71,21 @@ type RandomResponse struct {
 	message string
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	res := returnRandomResponse(&rng, ratio)
-	w.WriteHeader(res.status)
-	fmt.Fprint(w, res.message)
+func myHandler(w http.ResponseWriter, r *http.Request) {
+	printStats(w)
 }
 
-func randomHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func randomHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Do random response generation here
 		res := returnRandomResponse(&rng, ratio)
 		w.WriteHeader(res.status)
 		fmt.Fprint(w, res.message)
-
-		// call other function here
-		fn(w, r)
-	}
+	})
 }
 
-func slowHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func slowHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var sr RandomResponse
 		if slow {
 			sr = returnSlowResponse(slow_duration)
@@ -93,7 +93,7 @@ func slowHandler() http.HandlerFunc {
 			sr = returnSlowResponse(0)
 		}
 		fmt.Fprintf(w, sr.message)
-	}
+	})
 }
 
 func returnRandomResponse(e EntropySource, r int) RandomResponse {
